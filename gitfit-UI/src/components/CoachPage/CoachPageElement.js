aa2258fc5a85c48d968bcc3ac43e7a3cf20f0dc2
AndreeaCvl <44116789+AndreeaCvl@users.noreply.github.com>
2022-12-12 13:15:30 +0200

    add coach page availability

--- gitfit-UI/src/components/CoachPage/CoachPageElement.js
+++ gitfit-UI/src/components/CoachPage/CoachPageElement.js
@@ -10,19 +10,41 @@ import './CoachPageElement.css';
 
 
 export default function CoachPageElement() {
+
+    const [date, setDate] = useState(new Date());
+
+    const onDateChange = (newDate) => {
+        setDate(newDate);
+        console.log(newDate);
+    }
+
+    const current = new Date();
+    const currentDate = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+
     return(
 
         <div className="CoachUserPageWrapper">
             <div className="CoachUserPageLeft">
+
                 <div className="clientProfileWrapper">
                     <div className="profilePhoto"><img className="photo" src = {userPhoto}></img></div>
                     <div className="clientUsername">wade887</div>
                     <div className="clientName" style = {{fontWeight: 900, paddingLeft: "5px"}}>Wade Warrens</div>
                 </div>
+
+                <div className="coachPersonalInfo">Schedule a Session With Ronald</div>
+                <div className="clientPageCalendar"><Calendar onChange={onDateChange} value={date} /></div>
+
             </div>
 
             <div className="CoachUserPageRight">
-                a
+                <div className="coachPageAvailability">Availability</div>
+                <div className="availabilityList">
+                    <div className="availabilityDate">22 dec 2022, 10:00 AM</div>
+                    <div className="availabilityDate">22 dec 2022, 10:00 AM</div>
+                    <div className="availabilityDate">22 dec 2022, 10:00 AM</div>
+                </div>
             </div>
         </div>
     );
